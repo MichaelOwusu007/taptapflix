39eab1130136c1a6dfa753c86e0fec7b16f9215e
MichaelOwusu <nanayawwiafe007@gmail.com>
2024-01-22 06:15:17 +0000

    taptapflix

--- src/App.js
+++ src/App.js
@@ -1,24 +1,38 @@
-import logo from './logo.svg';
-import './App.css';
+
+import React, { useState } from 'react';
+import Home from "./components/Home";
+import "./styles/App.css";
+import Contact from "./pages/Contact";
+import Aboutus from "./pages/AboutUs";
+import Movies from "./pages/Movies";
+import Navbar from './components/Navbar';
+import Search from './components/Search';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 function App() {
+  const [searchResults, setSearchResults] = useState([]);
+
+  
+  const updateSearchResults = (results) => {
+    setSearchResults(results);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+
+      <Routes>
+        <Route path="/aboutus" element={<Aboutus />} />
+        <Route path="/contact" element={<Contact />} />
+        <Route path="/movies" element={<Movies />} />
+        <Route path="/" element={<Home />} />
+
+      
+        <Route path="/search/:query" element={<Search searchResults={searchResults} />} />
+      </Routes>
+
+    
+      <Navbar updateSearchResults={updateSearchResults} />
+    </Router>
   );
 }
 
